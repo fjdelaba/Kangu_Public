76af4214491e8752d5e0912064baa658c20c8cbb
Esteban Loaiza <eloaiza@dlb.cl>
2022-04-18 14:06:41 -0400

    Boton COmponent

---
+++ src/components/configuracion/mantenedores/Boton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button>
+        <icon>{{ icon }}</icon>
+        {{ text }}
+        </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BotonMantenedor',
+  props: {
+    text,
+    icon,
+    link
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/configuracion/mantenedores/Listado.vue
+++ src/pages/configuracion/mantenedores/Listado.vue
@@ -1,12 +1,19 @@
 <template>
   <h2>
     Listado Mantenedores A B C
+    <li>
+      
+    </li>
+
   </h2>
 </template>
 
+import 
 <script>
 export default {
+  components: {
 
+  }
 }
 </script>
 
