0a55f6641a1524520e65175fbc410c120cfd8a07
Esteban Loaiza <eloaiza@dlb.cl>
2022-04-19 01:21:01 -0400

    test configs vars heroku

--- src/auth/auth0-plugin.js
+++ src/auth/auth0-plugin.js
@@ -67,13 +67,16 @@ export const useAuth0 = ({
     async created() {
       this.auth0Client = await createAuth0Client({
         ...pluginOptions,
-        domain: process.env.AUTH_DOMAIN || 'kangusoft.us.auth0.com',//pluginOptions.domain,
-        client_id: process.env.AUTH_CLIENT_ID || 'vrEYoiels6SlT3fgKo1NWso3f9JMU5Z8',//pluginOptions.clientId,
+
+        // Environment Heroku
+        domain: process.env.VUE_APP_AUTH_DOMAIN || 'kangusoft.us.auth0.com',//pluginOptions.domain,
+        client_id: process.env.VUE_APP_AUTH_CLIENT_ID || 'vrEYoiels6SlT3fgKo1NWso3f9JMU5Z8',//pluginOptions.clientId,
         audience: pluginOptions.audience,
         //redirect_uri: redirectUri
         // redirect_uri: 'http://localhost:8080/dashboard/analytics'
-        redirect_uri: process.env.URL_POST_LOGIN || 'http://localhost:8080/dashboard/analytics'
+        redirect_uri: process.env.VUE_APP_URL_POST_LOGIN || 'http://localhost:8080/dashboard/analytics'
         // redirect_uri: 'https://kangu.cl/dashboard/analytics'
+
       })
 
       try {
