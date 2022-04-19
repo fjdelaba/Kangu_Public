f37ae7d518295006c9bc676f6e45a874498146fa
Esteban Loaiza <eloaiza@dlb.cl>
2022-04-19 10:43:10 -0400

    Commit para heroku

--- src/components/configuracion/mantenedores/Boton/Boton.vue
+++ src/components/configuracion/mantenedores/Boton/Boton.vue
@@ -1,39 +1,39 @@
 <template>
   <div>
     <v-row no-gutters>
-       <v-col order="12">
+      <v-col order="12">
         <div class="div2">
-          <v-btn  color="success" fab x-large dark>
+          <v-btn color="success" fab x-large dark>
             <v-icon>mdi-currency-usd</v-icon>
           </v-btn>
           <p>Moneda</p>
         </div>
-     </v-col>
-       <v-col order="12">
+      </v-col>
+      <v-col order="12">
         <div class="div2">
           <v-btn color="success" fab x-large dark>
             <v-icon>mdi-account-hard-hat</v-icon>
           </v-btn>
           <p>Estado Centro de Gestión</p>
         </div>
-          </v-col>
-       <v-col order="12">
+      </v-col>
+      <v-col order="12">
         <div class="div2">
           <v-btn color="success" fab x-large dark>
             <v-icon>mdi-briefcase-search</v-icon>
           </v-btn>
           <p>Unidades</p>
         </div>
-          </v-col>
-       <v-col>
+      </v-col>
+      <v-col>
         <div class="div2">
           <v-btn color="success" fab x-large dark>
             <v-icon>mdi-cash-multiple</v-icon>
           </v-btn>
           <p>Forma de Pago</p>
         </div>
-          </v-col>
-       <v-col>
+      </v-col>
+      <v-col>
         <div class="div2">
           <v-btn color="success" fab x-large dark>
             <v-icon>mdi-truck-check</v-icon>
