0b070ec016751652a736990065a2d49ca2211262
Esteban Loaiza <eloaiza@dlb.cl>
2022-04-19 00:43:35 -0400

    test heroku

--- src/pages/landing/HomePage.vue
+++ src/pages/landing/HomePage.vue
@@ -47,7 +47,6 @@ export default {
     CallToAction
   },
   created() {
-    console.log(process.env.NODE_ENV.URL_POST_LOGIN)
     console.log(process.env.URL_POST_LOGIN)
     console.dir(process)
   }
