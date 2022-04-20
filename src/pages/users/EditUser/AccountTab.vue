71d1bcd521e2fc772ba51a9d269c0fb2124915c0
FelipeAllendes <felipe.allendes.perez@gmail.com>
2022-04-20 16:26:09 -0400

    avance

--- src/pages/users/EditUser/AccountTab.vue
+++ src/pages/users/EditUser/AccountTab.vue
@@ -136,10 +136,10 @@
              <v-card
                :loading="loading"
                class=" my-2"
-                max-width="187">
+                max-width="215">
     <template slot="progress">
       <v-progress-linear
-        color="deep-purple"
+        color="deep-gray"
         height="5"
         indeterminate
       ></v-progress-linear>
@@ -164,14 +164,52 @@
     <v-card-text>
       <v-chip-group
         v-model="selection"
-        active-class="deep-purple accent-4 white--text"
+        active-class="deep-gray accent-4 white--text"
         column
       >
-        <v-chip>Comprador</v-chip>
+      <center>
+        <v-chip>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                  color="green darken-1"
+                  dark
+                  v-bind="attrs"
+                   v-on="on">
+                    mdi-cash
+                 </v-icon>
+             </template>
+             <span>Comprador</span>
+            </v-tooltip></v-chip>
 
-        <v-chip>Aprobador</v-chip>
+        <v-chip>
+          <v-tooltip bottom>
+             <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                  color="blue darken-4"
+                  dark
+                  v-bind="attrs"
+                   v-on="on">
+                    mdi-check-circle
+                 </v-icon>
+             </template>
+              <span>Aprobador</span>
+            </v-tooltip></v-chip>
 
-        <v-chip>Solicitante</v-chip>
+        <v-chip>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+                <v-icon
+                  color="black darken-1"
+                  dark
+                  v-bind="attrs"
+                   v-on="on">
+                    mdi-account
+                 </v-icon>
+            </template>
+             <span>Solicitante</span>
+            </v-tooltip></v-chip>
+        </center>
 
       </v-chip-group>
     </v-card-text>
