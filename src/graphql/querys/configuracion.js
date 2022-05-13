28e46d8acdca506c4a8d1d63400e45edbcafd9f9
Esteban Loaiza <eloaiza@dlb.cl>
2022-05-13 12:30:14 -0400

    se agrega usuario a carga inical de combos en proyecto

--- src/graphql/querys/configuracion.js
+++ src/graphql/querys/configuracion.js
@@ -1,8 +1,8 @@
 import gql from 'graphql-tag'
 
 const GET_DATOS_GENERALES = gql`
-    query Q_GET_DATOS_GENERALES {
-    kangusoft_emp_mon(where: {activo: {_eq: true}}) {
+  query Q_GET_DATOS_GENERALES {
+    kangusoft_emp_mon(where: { activo: { _eq: true } }) {
       mon {
         nombre
         id
@@ -12,13 +12,19 @@ const GET_DATOS_GENERALES = gql`
       nombre
       id
     }
-    kangusoft_pro_uni(where: {activo: {_eq: true}}) {
+    kangusoft_pro_uni(where: { activo: { _eq: true } }) {
       nombre
       id
     }
-    kangusoft_fla(where: {activo: {_eq: true}, fla_amb_fk: {_eq: "1"}}) {
+    kangusoft_fla(where: { activo: { _eq: true }, fla_amb_fk: { _eq: "1" } }) {
       id
       nombre
+    }
+    kangusoft_usu {
+      apellidos
+      email
+      nombre
+      id
     }
   }
 `
