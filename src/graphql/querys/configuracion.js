bdd4b3b1dadd742f7ea73f115f8dbff2d590098d
Esteban Loaiza <eloaiza@dlb.cl>
2022-05-13 10:34:05 -0400

    GET_DATOS_GENERALES graph

--- src/graphql/querys/configuracion.js
+++ src/graphql/querys/configuracion.js
@@ -1,2 +1,26 @@
-// archivo base para las querys de configuracion
-const arr = () => {}
+import gql from 'graphql-tag'
+
+const GET_DATOS_GENERALES = gql`
+    query Q_GET_DATOS_GENERALES {
+    kangusoft_emp_mon(where: {activo: {_eq: true}}) {
+      mon {
+        nombre
+        id
+      }
+    }
+    kangusoft_pro_est {
+      nombre
+      id
+    }
+    kangusoft_pro_uni(where: {activo: {_eq: true}}) {
+      nombre
+      id
+    }
+    kangusoft_fla(where: {activo: {_eq: true}, fla_amb_fk: {_eq: "1"}}) {
+      id
+      nombre
+    }
+  }
+`
+
+export { GET_DATOS_GENERALES }
