f940787634d50e674a1495e675001b57c9dba7b5
Esteban Loaiza <eloaiza@dlb.cl>
2022-07-03 12:54:14 -0400

    usuario detalle

---
+++ src/components/configuracion/usuario/detalle/Detalle.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="flex-grow-1">
+    <div class="d-flex align-center">
+      <div>
+        <!-- <div class="display-1">Edit User {{ `${ usuario && usuario.nombre}  ${ usuario && usuario.apellidos}` }}</div> {{ user.name && `- ${user.name}` }} -->
+        <v-breadcrumbs :items="breadcrumbs" class="pa-0"></v-breadcrumbs>
+      </div>
+      <v-spacer></v-spacer>
+      <!-- <v-btn icon @click>
+        <v-icon>mdi-refresh</v-icon>
+      </v-btn> -->
+    </div>
+    <!-- disable  v-card -->
+    <datos ref="tabs-account" :user="usuario"></datos>
+  </div>
+</template>
+
+<script>
+// import CopyLabel from '../../components/common/CopyLabel'
+import CopyLabel from '../../../common/CopyLabel.vue'
+import Datos from './datos/Datos.vue'
+import gql from 'graphql-tag'
+
+const QUERY_USER = gql`
+query MyQuery($id_usuario: bigint!) {
+  kangusoft_usu(where: {id: {_eq: $id_usuario}}) {
+    apellidos
+    activo
+    avatar
+    cargo
+    email
+    firma
+    nombre
+    rut
+    # cgs(where: {estado_fk: {_eq: 1}}) {
+    #   nombre
+    #   id,
+    #   descripcion,
+    #   imagen
+    # }
+  }
+}
+
+`
+
+export default {
+  name: 'DetalleUsuario',
+  components: {
+    Datos
+  },
+  data() {
+    return {
+      user: {
+        'id':32,
+        'rut':123456789,
+        'email':'bfitchew0@ezinearticles.com',
+        'name':'Bartel ',
+        'lastname':' Fitchew',
+        'verified':false,
+        'created':'2019-08-09T03:14:12Z',
+        'lastSignIn':'2019-08-14T20:00:53Z',
+        'disabled':true,
+        'role':'ADMIN',
+        'avatar':'/images/avatars/avatar1.svg'
+      },
+      usu_id: {},
+      usuario: {},
+      tab: null,
+      breadcrumbs: [
+        {
+          text: 'Usuarios',
+          to: '/configuracion/usuarios',
+          exact: true
+        },
+        {
+          text: 'Detalle de Usuario'
+        }
+      ]
+    }
+  },
+  mounted() {
+    console.log('MOUNTED DETALLE USUARIO: ', this.$store.state.app.datosUsuario.user_id)
+    console.log('router: ', this.$route.query.id)
+
+    // this.usu_id = this.$auth.user['https://kangusoft.cl/jwt/hasura'] && this.$auth.user['https://kangusoft.cl/jwt/hasura'].user_id
+    this.usu_id = this.$store.state.app.datosUsuario.user_id
+    this.cargarDatosUsuario(this.$route.query.id)
+  },
+  methods: {
+    async cargarDatosUsuario(usu_id) {  
+      console.log('usu_id: ', usu_id)
+      const { data }  = await this.$apollo.query({
+        query: QUERY_USER,
+        variables:{
+          'id_usuario': usu_id
+        }
+      })
+
+      this.usuario = data.kangusoft_usu[0]
+      console.log('data data: ', data.kangusoft_usu[0])
+    }
+  }
+}
+</script>
---
+++ src/components/configuracion/usuario/detalle/datos/Datos.vue
@@ -0,0 +1,334 @@
+<template>
+  <div class="my-2">
+    <div>
+      <v-card>
+        <v-card-title>Información del usuario</v-card-title>
+        <v-card-text>
+          <div class="d-flex flex-column flex-sm-row">
+            <div>
+              <v-img
+                :src="user.avatar"
+                aspect-ratio="1"
+                class="blue-grey lighten-4 rounded elevation-3"
+                max-width="90"
+                max-height="90"
+              ></v-img>
+              <v-btn class="mt-1" small>Editar Avatar</v-btn>
+            </div>
+            <div class="flex-grow-1 pt-2 pa-sm-2">
+            
+              <v-text-field
+                v-model="user.nombre"
+                dense
+                label="Nombre"
+                placeholder="Nombre"
+                readonly
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="user.apellidos"
+                label="Apellido"
+                placeholder="Apellido Paterno"
+                readonly
+                dense
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="user.rut"
+                label="Rut"
+                placeholder="12.345.678-9"
+                readonly
+                dense
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="user.cargo"
+                label="Cargo"
+                readonly
+                dense
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-model="user.email"
+                label="Email"
+                readonly
+                dense
+                outlined
+              ></v-text-field>
+              <v-text-field
+                v-if="false"
+                v-model="user.clave"
+                type="password"
+                label="Clave"
+                readonly
+              ></v-text-field>
+              <v-text-field
+                v-if="false"
+                v-model="user.clave2"
+                type="password"
+                label="Confirmar clave"
+                readonly
+              ></v-text-field>
+            
+              <!-- <div class="d-flex flex-column">
+                <v-checkbox v-model="user.verified" dense label="Email Verified"></v-checkbox>
+                <div>
+                  <v-btn 
+                    v-if="false"
+                  >
+                    <v-icon left small>mdi-email</v-icon>Send Verification Email
+                  </v-btn>
+                </div>
+              </div> -->
+
+              <div class="mt-2">
+                <v-btn color="primary" small @click>Guardar</v-btn>
+              </div>
+              <div class="mt-2">
+                <v-btn color="primary" small @click>Editar</v-btn>
+              </div>
+            </div>
+          </div>
+        </v-card-text>
+      </v-card>
+
+      <v-expansion-panels v-model="panel" multiple class="mt-3">
+        <v-expansion-panel>
+          <v-expansion-panel-header class="title">Permisos</v-expansion-panel-header>
+          
+          <v-expansion-panel-content>
+            <v-row
+              v-for="n in 1 "
+              :key="n"
+              :class="n === 1 ? 'my-1' : ''"
+              no-gutters
+            >
+              <v-col md="4">     
+                <v-card
+                  :loading="loading"
+                  class=" my-1"
+                  max-width="400"
+                  outlined
+                  tile
+                >
+                  <v-card-title class="grey lighten-3">Panel de configuración</v-card-title>
+                  <v-container class="grey lighten-4">
+  
+                    <v-switch
+                      v-model="switch1"
+                      :label="`Centro de Gestión`"
+                      inset
+                    ></v-switch>
+                    <v-divider class="mx-4"></v-divider>
+  
+                    <v-switch
+                      v-model="switch2"
+                      :label="`Maestro de recursos`"
+                      inset
+                    ></v-switch>
+                    <v-divider class="mx-4"></v-divider>
+  
+                    <v-switch
+                      v-model="switch3"
+                      :label="`Entidad Externa`"
+                      inset
+                    ></v-switch>
+  
+                    <v-divider class="mx-4"></v-divider>
+  
+                    <v-switch
+                      v-model="switch4"
+                      inset
+                      :label="`Usuarios`"
+                    ></v-switch>
+                    <v-divider class="mx-4"></v-divider>
+  
+                    <v-switch
+                      v-model="switch4"
+                      inset
+                      :label="`Mantenedores`"
+                    ></v-switch>
+                  </v-container>
+                </v-card>
+              </v-col>
+              <v-col md="4"> 
+                <v-card
+                  :loading="loading"
+                  class="my-1"
+                  max-width="250"
+                  outlined="tile"
+                >
+                  <template slot="progress">
+                    <v-progress-linear
+                      color="deep-purple"
+                      height="10"
+                      indeterminate
+                    ></v-progress-linear>
+                  </template>
+                  <v-card-title class="grey lighten-3" >Adquisiciones</v-card-title>
+                  <v-container class="grey lighten-4">
+                    <v-switch
+                      v-model="switch5"
+                      label="Pedidos"
+                      inset
+                    ></v-switch>
+                    <v-divider class="mx-4"></v-divider>
+                    <v-switch
+                      v-model="switch7"
+                      label="Cotización "
+                      inset
+                    ></v-switch>
+                    <v-divider class="mx-4"></v-divider>
+                    <v-switch
+                      v-model="switch7"
+                      label="Orden de compras"
+                      inset
+                    ></v-switch>
+                    <v-divider class="mx-4"></v-divider>
+                    <v-switch
+                      v-model="switch7"
+                      label=" Despacho"
+                      inset
+                    ></v-switch>
+                    <v-divider class="mx-4"></v-divider>
+                    <v-switch
+                      v-model="switch7"
+                      label=" Recepcion"
+                      inset
+                    ></v-switch>
+                  
+                  </v-container>
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-expansion-panel-content>
+         
+        </v-expansion-panel>
+
+        <v-expansion-panel v-model="panel" multiple class="mt-3">
+          <v-expansion-panel-header class="title">Proyectos</v-expansion-panel-header>
+          <v-expansion-panel-content class="body-2" >
+            <!-- <card-proyecto :proyectos="user.cgs"></card-proyecto> -->
+            <v-card
+              :loading="loading"
+              class=" my-2"
+              max-width="200"
+            >
+              <template slot="progress">
+                <v-progress-linear
+                  color="deep-gray"
+                  height="5"
+                  indeterminate
+                ></v-progress-linear>
+              </template>
+
+              <v-img
+                height="140"
+                src="https://cache.enlaceinmobiliario.cl/Blog/noticianorteparquebrasilantofagasta_5bd7267aaf69c0.png"
+              ></v-img>
+
+              <v-card-title>Remodelación
+                <br>Parque Arauco</v-card-title> 
+              <v-card-text>
+                <div>Mejoras de veredas y áreas exteriores</div>
+              </v-card-text>
+
+              <v-divider class="mx-2"></v-divider>
+
+              <v-card-subtitle class="font-weight-black"> Roles (Accesos)</v-card-subtitle>
+
+              <v-card-text>
+                <v-chip-group
+                  v-model="selection"
+                  active-class="deep-gray accent-4 white--text"
+                  column
+                >
+                  <center>
+                    <v-chip >
+                      <v-tooltip bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-icon
+              
+                            color="green darken-1"
+                            dark
+                            v-bind="attrs"
+                            v-on="on"
+                          >
+                            mdi-cash
+                          </v-icon>
+                        </template>
+                        <span>Comprador</span>
+                      </v-tooltip></v-chip>
+
+                    <v-chip>
+                      <v-tooltip bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-icon
+                            color="blue darken-4"
+                            dark
+                            v-bind="attrs"
+                            v-on="on"
+                          >
+                            mdi-check-circle
+                          </v-icon>
+                        </template>
+                        <span>Aprobador</span>
+                      </v-tooltip></v-chip>
+
+                    <v-chip>
+                      <v-tooltip bottom>
+                        <template v-slot:activator="{ on, attrs }">
+                          <v-icon
+                            color="black darken-1"
+                            dark
+                            v-bind="attrs"
+                            v-on="on"
+                          >
+                            mdi-account
+                          </v-icon>
+                        </template>
+                        <span>Solicitante</span>
+                      </v-tooltip></v-chip>
+                  </center>
+
+                </v-chip-group>
+              </v-card-text>
+            </v-card> 
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels></div>
+  </div>
+</template>
+
+<script>
+
+// import CardProyecto from '../../../components/configuracion/usuario/card-proyecto/CardProyecto.vue'
+import CardProyecto from '../../../../configuracion/usuario/card-proyecto/CardProyecto.vue'
+
+export default {
+  components: {
+    
+  },
+  props: {
+    user: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  data() {
+    return {
+      panel: [-1],
+      switch1: true,
+      switch2: true,
+      switch3: true,
+      switch4: true,
+      switch5: true,
+      switch6: true,
+      switch7: true,
+      deleteDialog: false,
+      disableDialog: false
+     
+    }
+  }
+}
+</script>
