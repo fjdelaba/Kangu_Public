45aaab2967837094f3f94eb4207ad97bfd71a08b
Felipe Allendes <felipe.allendes.perez@gmail.com>
2022-07-12 13:20:44 -0400

    mi empresa

--- src/pages/MiEmpresa.vue
+++ src/pages/MiEmpresa.vue
@@ -30,90 +30,65 @@
               :rules="usuarioRules.nombreRules"
             >
             </v-text-field>
-            <v-row>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="6">
+            <v-row> 
             <v-text-field
               label="Rut"
               placeholder="12.345.678-9"
               dense
               outlined
+              class="pl-3 pr-2"
               :rules="usuarioRules.rutRules"
             >
             </v-text-field>
-            </v-col>
-             <v-col
-                  cols="12"
-                  sm="6"
-                  md="6">
+            
              <v-text-field     
               v-model="usuario.representante"
               label="Representante"
               dense
               outlined
+              class="pl-2 pr-3"
               :rules="usuarioRules.representanteRules"
             ></v-text-field>
-            </v-col>
             </v-row>
-            <v-row
-             >
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="6">
+
+            <v-row>
             <v-text-field
               v-model="usuario.telefono"
               label="Telefono"
               dense
+              class="pl-3 pr-2"
               outlined
               :rules="usuarioRules.telefonoRules"
             ></v-text-field>
-            </v-col>
-            <v-col
-                  cols="12"
-                  sm="6"
-                  md="6">
             <v-text-field
               v-model="usuario.email"
               label="Email"
               dense
+              class="pl-2 pr-3"
               outlined
               :rules="usuarioRules.emailRules"
             ></v-text-field>
-            </v-col>
             </v-row>
+
             <v-row>
-                <v-col
-                  cols="12"
-                  sm="6"
-                  md="6">
             <v-select
               :items="items"
               :rules="[(v) => !!v || 'La Region es obligatoria']"
               label="Región"
-              required
               dense
-              style="width:100%"
+              class="pl-3 pr-2"
               outlined
             ></v-select>
-            </v-col>
-           
-            <v-col
-                  cols="12"
-                  sm="6"
-                  md="6">
+            
             <v-select
               :items="items2"
               :rules="[(v) => !!v || 'La comuna es obligatoria']"
               label="Comuna"
               required
               dense
-              style="width:100%"
+              class="pl-2 pr-3"
               outlined
             ></v-select>
-            </v-col>
             </v-row>
              <v-text-field
               v-model="usuario.direccion"
@@ -129,7 +104,6 @@
               label="Giro"
               dense
               outlined
-              class="mt-2"
               :rules="usuarioRules.giroRules"
             ></v-text-field>
 
@@ -155,15 +129,20 @@
               no-gutters
             >
              <v-col md="4">   
-          <v-card class="mx-auto mb-3" max-width="344">
+          <v-card class="mx-auto mb-3 py-2" max-width="344">
             <v-img
               src="../assets/images/plan1.png"
-              height="140px"
-              contain
+               max-height="140px"
+              contain 
+              
             ></v-img>
 
             <v-card-title> Plan Básico </v-card-title>
-
+             <v-divider class="mx-5"></v-divider>
+           <v-card-text>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus hic amet laudantium labore voluptates, laborum, magnam reprehenderit in corrupti, molestias perferendis repellat! Voluptas aliquam rem illo culpa ab? Molestiae, iure.</div>
+    </v-card-text>
+             
             <v-card-actions>
               <v-btn color="primary" text> Ver Plan </v-btn>
 
@@ -180,22 +159,36 @@
               <div v-show="show">
                 <v-divider></v-divider>
 
-                <v-card-text>Cantidad de usuarios: <br><br>Cantidad de usuarios concurrentes: <br><br> Modulos: <br> <br> Precios: </v-card-text>
+                <v-card-text class="font-weight-bold">Cantidad de usuarios: <br><br>Cantidad de usuarios concurrentes: <br><br> Modulos: <br> <br> Precios: </v-card-text>
               </div>
             </v-expand-transition>
+              <div 
+             class="text-center">
+          <v-btn
+            text
+            color="primary"
+            @click="reveal = false"
+          >
+            Seleccionar
+          </v-btn>
+           </div>
           </v-card>
 
           </v-col>
           <v-col md="4"> 
-          <v-card class="mx-auto mb-3" max-width="344">
+          <v-card class="mx-auto mb-3 py-2" max-width="344">
             <v-img
+               class=" m-y 2"
               src="../assets/images/plan2.png"
               height="140px"
               contain
             ></v-img>
 
             <v-card-title> Plan Intermedio </v-card-title>
-
+             <v-divider class="mx-5"></v-divider>
+            <v-card-text>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus hic amet laudantium labore voluptates, laborum, magnam reprehenderit in corrupti, molestias perferendis repellat! Voluptas aliquam rem illo culpa ab? Molestiae, iure.</div>
+    </v-card-text>
             <v-card-actions>
               <v-btn color="primary" text> Ver Plan </v-btn>
 
@@ -212,21 +205,37 @@
               <div v-show="show1">
                 <v-divider></v-divider>
 
-                <v-card-text> Cantidad de usuarios: <br><br>Cantidad de usuarios concurrentes: <br><br> Modulos: <br> <br> Precios:</v-card-text>
+                <v-card-text class="font-weight-bold"> Cantidad de usuarios: <br><br>Cantidad de usuarios concurrentes: <br><br> Modulos: <br> <br> Precios:</v-card-text>
               </div>
             </v-expand-transition>
+              <div 
+             class="text-center">
+          <v-btn
+            text
+            color="primary"
+            @click="reveal = false"
+          >
+            Seleccionar
+          </v-btn>
+           </div>
           </v-card>
 
           </v-col>
           <v-col md="4"> 
-          <v-card class="mx-auto mb-3" max-width="344">
+          <v-card class="mx-auto mb-3 py-2" max-width="344">
             <v-img
+              
               src="../assets/images/plan3.png"
               height="140px"
               contain
+              class=" m-y 2"
             ></v-img>
 
             <v-card-title> Plan Avanzado </v-card-title>
+             <v-divider class="mx-5"></v-divider>
+            <v-card-text>
+      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus hic amet laudantium labore voluptates, laborum, magnam reprehenderit in corrupti, molestias perferendis repellat! Voluptas aliquam rem illo culpa ab? Molestiae, iure.</div>
+    </v-card-text>
 
             <v-card-actions>
               <v-btn color="primary" text> Ver Plan </v-btn>
@@ -244,9 +253,20 @@
               <div v-show="show2">
                 <v-divider></v-divider>
 
-                <v-card-text>Cantidad de usuarios: <br><br>Cantidad de usuarios concurrentes: <br><br> Modulos: <br> <br> Precios: </v-card-text>
+                <v-card-text class="font-weight-bold">Cantidad de usuarios: <br><br>Cantidad de usuarios concurrentes: <br><br> Modulos: <br> <br> Precios: </v-card-text>
               </div>
             </v-expand-transition>
+           
+             <div 
+             class="text-center">
+          <v-btn
+            text
+            color="primary"
+            @click="reveal = false"
+          >
+            Seleccionar
+          </v-btn>
+           </div>
           </v-card>
           </v-col>
          </v-row>
