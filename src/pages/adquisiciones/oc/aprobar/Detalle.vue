cdcc6e124fb4165aa750e7dd8038dce2fa0e49ae
Esteban Loaiza <eloaiza@dlb.cl>
2022-07-14 13:02:24 -0400

    correcion aprobacion y descarga pdf

--- src/pages/adquisiciones/oc/aprobar/Detalle.vue
+++ src/pages/adquisiciones/oc/aprobar/Detalle.vue
@@ -10,13 +10,14 @@
         class="flex-grow-1 flex-shrink-1 w-full"
       >
         <div>
-          <h2>Detalle Aprobar</h2>
+          <h2>Detalle Aprobar </h2>
           <previsualizacion
             :materiales="materialesOcSeleccionada"
             :cabecera="cabecera"
             observacion=""
             :aprobacion="aprobacion"
             :aprobadores="aprobadores"
+            :tipo_documento="tipo_documento"
           ></previsualizacion>
         </div>
       </v-col>
